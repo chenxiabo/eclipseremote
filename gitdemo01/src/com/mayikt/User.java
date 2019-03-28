45deadc90c1fd824b243fe1405f25474205c9b73
cxb <514617445@qq.com>
2019-03-28 18:15:04 +0800

    2222

--- gitdemo01/src/com/mayikt/User.java
+++ gitdemo01/src/com/mayikt/User.java
@@ -1,13 +1,8 @@
 package com.mayikt;
 
 public class User {
-<<<<<<< HEAD
 	//123	
-private int i = 105;
-=======
-	//123
-private int i = 106;
->>>>>>> branch 'master' of https://github.com/chenxiabo/eclipseremote.git
+private int i = 100;
 	
 //定义变量
 	int b = 99;
